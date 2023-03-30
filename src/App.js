1468affcc3a96e64b03d73dfd899b5e5d72a4ce8
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-30 20:52:03 +0530

    modify file

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Welcome to Redux
+    <div className='container'>
+
+      <h1>Increment/Decrement Counter</h1>
+      <h4>using React and Redux</h4>
+      
     </div>
   );
 }
