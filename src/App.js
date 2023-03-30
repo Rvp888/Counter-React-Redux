a6ddf639e548aa0798b628eb1dec86be6bc430ed
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-30 23:45:13 +0530

    modify file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import { useSelector } from 'react-redux';
 
 
 
+
 function App() {
+
+  const myState = useSelector((store) => store.changeTheNumber);
+
   return (
     <div className='container'>
 
@@ -13,7 +17,7 @@ function App() {
 
       <div className='quantity'>
         <a className='quantity_minus' title='Decrement' ><span>-</span></a>
-        <input name='quantity' type='text' className='quantity_input' value={0} />
+        <input name='quantity' type='text' className='quantity_input' value={myState} />
         <a className='quantity_plus' title='Increment' ><span>+</span></a>
       </div>
       
